ac6a6c74f9c14a9c6f3e1b1fd9a1b093792c96f0
Roys wanimbo <157364709+royswanimbo@users.noreply.github.com>
2024-08-29 20:39:12 +0900

    commit baru

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// single line commet
+
+/*
+multiple line commet
+bisa tulis juga di sini
+*/
+
+var namaDepan = "Roys";
+var namaBalakang = "Wanimbo";
+document.getElementById.apply("nama").innerHTML = namaDepan + namaBalakang;
